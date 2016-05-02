dce28d20cc9716e6cf9c7d242f8faeff80c8ba36
jakub.gwozdz@amartus.com <jakub.gwozdz@amartus.com>
2016-05-02 12:07:39 +0200

    tokenizer working

--- src/pl/jgwozdz/intellij/unitscalc/Calculator.kt
+++ src/pl/jgwozdz/intellij/unitscalc/Calculator.kt
@@ -2,7 +2,6 @@ package pl.jgwozdz.intellij.unitscalc
 
 import java.math.BigDecimal
 import java.text.NumberFormat
-import java.text.ParseException
 import java.util.*
 
 
@@ -16,12 +15,12 @@ enum class Units { MM, CM, IN, PX, PT }
 
 interface Expression
 
-data class Addition(val term1: Term, val term2: Expression) : Expression
-data class Subtraction(val minuend: Term, val substrahend: Expression) : Expression
+data class Addition(val term1: Expression, val term2: Term) : Expression
+data class Subtraction(val minuend: Expression, val substrahend: Term) : Expression
 interface Term : Expression
 
-data class Multiplication(val factor: BigDecimal, val expression: Measurement) : Term // future use
-data class Division(val expression: Measurement, val divisor: BigDecimal) : Term // future use
+//data class Multiplication(val factor: BigDecimal, val expression: Factor) : Term // future use
+//data class Division(val expression: Factor, val divisor: BigDecimal) : Term // future use
 interface Factor : Term
 
 data class Measurement(val amount: BigDecimal, val units: Units) : Factor
@@ -71,65 +70,205 @@ class ExpressionFormatter {
 
 }
 
+class ParseException(data:String, pos: Int, val detailedMessage : String) : java.text.ParseException(data, pos) {
+    override fun toString(): String{
+        val data = message?.substring(0, errorOffset) + "[]" + message?.substring(errorOffset)  
+        return "ParseException: $detailedMessage at pos $errorOffset: `$data`"
+    }
+}
+
+
 class ExpressionParser {
 
     fun parse(data: String): Expression {
         val tokens: MutableList<Token> = tokenize(data)
         for (token in tokens) {
+            
         }
         throw UnsupportedOperationException()
     }
 
-    private fun tokenize(data: String): MutableList<Token> {
-        val tokens: MutableList<Token> = mutableListOf()
-        var state = TokienizerState.START;
-        var value = StringBuilder()
-        for (c in data.toCharArray()) {
-            when (state) {
-                TokienizerState.START -> when {
-                    Character.isWhitespace(c) -> {
-                    }
-                    c == '(' -> {
-                        tokens.add(OpenBracket()); state = TokienizerState.START
-                    }
-                    c == '-' -> {
-                        value.append(c); state = TokienizerState.IN_NUMBER_JUST_AFTER_MINUS
-                    }
-                    Character.isDigit(c) -> {
-                        value.append(c); state = TokienizerState.IN_NUMBER_BEFORE_DOT
-                    }
-                    c == '.' -> {
-                        value.append(c) ; state = TokienizerState.IN_NUMBER_AFTER_DOT
-                    }
-                    else -> throw ParseException(data, 0)
-                }
-                TokienizerState.IN_NUMBER_JUST_AFTER_MINUS -> when {
-                    Character.isDigit(c) -> {
-                        value.append(c) ; state = TokienizerState.IN_NUMBER_BEFORE_DOT
-                    }
-                    c == '.' -> {
-                        value.append(c) ; state = TokienizerState.IN_NUMBER_AFTER_DOT
-                    }
-                    else -> throw ParseException(data, 0)
-                }
-                TokienizerState.IN_NUMBER_BEFORE_DOT -> when {
-                    Character.isWhitespace(c) -> { // TODO : unfinished
-                        tokens.add(NumberToken(BigDecimal(value.toString()))); value = StringBuilder(); state = TokienizerState.AFTER_NUMBER
-                    }
-                    Character.isDigit(c) -> {
-                        value.append(c) ; state = TokienizerState.IN_NUMBER_BEFORE_DOT
-                    }
-                    c == '.' -> {
-                        value.append(c) ; state = TokienizerState.IN_NUMBER_AFTER_DOT
-                    }
-                    else -> throw ParseException(data, 0)
-                }
-            }
-        }
+    fun tokenize(data: String): MutableList<Token> {
+        val tokens: MutableList<Token> = TokenizeProcess(data).process()
         return tokens
     }
 
-    enum class TokienizerState {START, IN_NUMBER_JUST_AFTER_MINUS, IN_NUMBER_BEFORE_DOT, IN_NUMBER_AFTER_DOT, IN_NUMBER_JUST_AFTER_DOT, AFTER_NUMBER, IN_UNITS, AFTER_UNITS }
+    class TokenizeProcess(val data: String) {
+        enum class TokienizerState {START, IN_NUMBER_JUST_AFTER_MINUS, IN_NUMBER_BEFORE_DOT, IN_NUMBER_AFTER_DOT, IN_NUMBER_JUST_AFTER_DOT, AFTER_NUMBER, IN_UNITS, AFTER_UNITS }
+        enum class CharClass { SPACE, DIGIT, MINUS, PLUS, POINT, LETTER, OPENBRACKET, CLOSEBRACKET }
+
+        private val tokens: MutableList<Token> = mutableListOf()
+        private var state = TokienizerState.START;
+        private var buffer = StringBuilder()
+
+        fun process(): MutableList<Token> {
+
+            for ((pos, c) in data.toCharArray().withIndex()) {
+                val charClass = when {
+                    c.isWhitespace() -> CharClass.SPACE
+                    c.isDigit() -> CharClass.DIGIT
+                    c.isLetter() -> CharClass.LETTER
+                    c == '(' -> CharClass.OPENBRACKET
+                    c == ')' -> CharClass.CLOSEBRACKET
+                    c == '-' -> CharClass.MINUS
+                    c == '+' -> CharClass.PLUS
+                    c == '.' -> CharClass.POINT
+                    else -> throw ParseException(data, pos, "Expected space, digit, letter, '(', ')', '-', '+' or '.'")
+                }
+                when (state) {
+                    TokienizerState.START -> when (charClass) {
+                        CharClass.SPACE -> {
+                            state = TokienizerState.START
+                        }
+                        CharClass.OPENBRACKET -> {
+                            tokens.add(OpenBracket())
+                            state = TokienizerState.START
+                        }
+                        CharClass.MINUS -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_NUMBER_JUST_AFTER_MINUS
+                        }
+                        CharClass.DIGIT -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_NUMBER_BEFORE_DOT
+                        }
+                        CharClass.POINT -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_NUMBER_JUST_AFTER_DOT
+                        }
+                        else -> throw ParseException(data, pos, "Expected space, digit, '(', '-', or '.'")
+                    }
+                    TokienizerState.IN_NUMBER_JUST_AFTER_MINUS -> when (charClass) {
+                        CharClass.DIGIT -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_NUMBER_BEFORE_DOT
+                        }
+                        CharClass.POINT -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_NUMBER_JUST_AFTER_DOT
+                        }
+                        else -> throw ParseException(data, pos, "Expected digit or '.'")
+                    }
+                    TokienizerState.IN_NUMBER_BEFORE_DOT -> when (charClass) {
+                        CharClass.DIGIT -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_NUMBER_BEFORE_DOT
+                        }
+                        CharClass.POINT -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_NUMBER_JUST_AFTER_DOT
+                        }
+                        CharClass.SPACE -> {
+                            finishNumberToken()
+                            state = TokienizerState.AFTER_NUMBER
+                        }
+                        CharClass.LETTER -> {
+                            finishNumberToken()
+                            buffer.append(c)
+                            state = TokienizerState.IN_UNITS
+                        }
+                        else -> throw ParseException(data, pos, "Expected space, digit, letter or '.'")
+                    }
+                    TokienizerState.IN_NUMBER_JUST_AFTER_DOT -> when (charClass) {
+                        CharClass.DIGIT -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_NUMBER_AFTER_DOT
+                        }
+                        else -> throw ParseException(data, pos, "Expected digit")
+                    }
+                    TokienizerState.IN_NUMBER_AFTER_DOT -> when (charClass) {
+                        CharClass.DIGIT -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_NUMBER_AFTER_DOT
+                        }
+                        CharClass.SPACE -> {
+                            finishNumberToken()
+                            state = TokienizerState.AFTER_NUMBER
+                        }
+                        CharClass.LETTER -> {
+                            finishNumberToken()
+                            buffer.append(c)
+                            state = TokienizerState.IN_UNITS
+                        }
+                        else -> throw ParseException(data, pos, "Expected space, digit or letter")
+                    }
+                    TokienizerState.AFTER_NUMBER -> when (charClass) {
+                        CharClass.SPACE -> {
+                            state = TokienizerState.AFTER_NUMBER
+                        }
+                        CharClass.LETTER -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_UNITS
+                        }
+                        else -> throw ParseException(data, pos, "Expected space or letter")
+                    }
+                    TokienizerState.IN_UNITS -> when (charClass) {
+                        CharClass.LETTER -> {
+                            buffer.append(c)
+                            state = TokienizerState.IN_UNITS
+                        }
+                        CharClass.SPACE -> {
+                            finishUnitsToken()
+                            state = TokienizerState.AFTER_UNITS
+                        }
+                        CharClass.PLUS -> {
+                            finishUnitsToken()
+                            tokens.add(Plus())
+                            state = TokienizerState.START
+                        }
+                        CharClass.MINUS -> {
+                            finishUnitsToken()
+                            tokens.add(Minus())
+                            state = TokienizerState.START
+                        }
+                        CharClass.CLOSEBRACKET -> {
+                            finishUnitsToken()
+                            tokens.add(CloseBracket())
+                            state = TokienizerState.AFTER_UNITS
+                        }
+                        else -> throw ParseException(data, pos, "Expected space, letter, ')', '-' or '+'")
+                    }
+                    TokienizerState.AFTER_UNITS -> when (charClass) {
+                        CharClass.SPACE -> {
+                            state = TokienizerState.AFTER_UNITS
+                        }
+                        CharClass.PLUS -> {
+                            tokens.add(Plus())
+                            state = TokienizerState.START
+                        }
+                        CharClass.MINUS -> {
+                            tokens.add(Minus())
+                            state = TokienizerState.START
+                        }
+                        CharClass.CLOSEBRACKET -> {
+                            tokens.add(CloseBracket())
+                            state = TokienizerState.AFTER_UNITS
+                        }
+                        else -> throw ParseException(data, pos, "Expected space, ')', '-' or '+'")
+                    }
+                }
+            }
+            when (state) {
+                TokienizerState.IN_UNITS -> finishUnitsToken()
+                TokienizerState.IN_NUMBER_BEFORE_DOT, TokienizerState.IN_NUMBER_AFTER_DOT -> finishNumberToken()
+                TokienizerState.AFTER_NUMBER, TokienizerState.AFTER_UNITS -> {}
+                else -> throw ParseException(data, data.length-1, "Unexpected end of data")
+            }
+            return tokens
+        }
+
+        private fun finishUnitsToken() {
+            val units = Units.valueOf(buffer.toString().toUpperCase())
+            tokens.add(UnitsToken(units))
+            buffer = StringBuilder()
+        }
+
+        private fun finishNumberToken() {
+            val amount = BigDecimal(buffer.toString())
+            tokens.add(NumberToken(amount))
+            buffer = StringBuilder()
+        }
+    }
 
     interface Token
     class OpenBracket : Token
@@ -137,7 +276,7 @@ class ExpressionParser {
     class Plus : Token
     class Minus : Token
     data class NumberToken(val value: BigDecimal) : Token
-    data class UnitsToken(val value: BigDecimal) : Token
+    data class UnitsToken(val value: Units) : Token
 
 }
 
--- test/pl/jgwozdz/intellij/unitscalc/CalculatorTest.kt
+++ test/pl/jgwozdz/intellij/unitscalc/CalculatorTest.kt
@@ -1,9 +1,10 @@
 package pl.jgwozdz.intellij.unitscalc
 
-import org.hamcrest.CoreMatchers.equalTo
-import org.junit.Assert.assertThat
+import org.hamcrest.CoreMatchers.*
+import org.junit.Assert.*
 import org.junit.Test
 import java.math.BigDecimal
+import java.text.ParseException
 
 /**
 
@@ -11,22 +12,22 @@ import java.math.BigDecimal
 class MeasurementFormatterTest {
     val formatter = ExpressionFormatter()
 
-    @Test fun toStringTest() {
+    @Test fun noLeadingZero() {
         val measurement = Measurement(BigDecimal(".4567"), Units.IN)
         assertThat(formatter.format(measurement), equalTo("0.457in"))
     }
 
-    @Test fun toStringLargeValueTest() {
+    @Test fun largeValue() {
         val measurement = Measurement(BigDecimal("123.4567"), Units.IN)
         assertThat(formatter.format(measurement), equalTo("123.457in"))
     }
 
-    @Test fun toStringNegativeTest() {
+    @Test fun negative() {
         val measurement = Measurement(BigDecimal("-.4567"), Units.IN)
         assertThat(formatter.format(measurement), equalTo("-0.457in"))
     }
 
-    @Test fun toStringRoundedTest() {
+    @Test fun roundNumber() {
         val measurement = Measurement(BigDecimal("1234"), Units.PX)
         assertThat(formatter.format(measurement), equalTo("1234px"))
     }
@@ -38,8 +39,62 @@ class ExpressionFormatterTest {
 
     val formatter = ExpressionFormatter()
 
-    @Test fun additionToStringTest() {
+    @Test fun additionWithSecondNegative() {
         val addition = Addition(Measurement(BigDecimal("123.4567"), Units.IN), Measurement(BigDecimal("-.4567"), Units.IN))
         assertThat(formatter.format(addition), equalTo("123.457in + (-0.457in)"))
     }
+    @Test fun additionWithBothNegative() {
+        val addition = Addition(Measurement(BigDecimal("-123.4567"), Units.IN), Measurement(BigDecimal("-.4567"), Units.IN))
+        assertThat(formatter.format(addition), equalTo("-123.457in + (-0.457in)"))
+    }
+}
+
+class TokenizeProcessTest {
+    val parser = ExpressionParser();
+
+    @Test(expected = ParseException::class) fun failOnEmpty() {
+        val tokens = parser.tokenize("")
+        assertThat(tokens.size, equalTo(0))
+    }
+
+    @Test fun simpleNumber() {
+        val tokens = parser.tokenize("123")
+        assertThat(tokens.size, equalTo(1))
+        assertThat((tokens[0] as ExpressionParser.NumberToken).value.toString(), equalTo("123"))
+    } 
+
+    @Test fun simpleMeasurement() {
+        val tokens = parser.tokenize("-123.4567in")
+        assertThat(tokens.size, equalTo(2))
+        assertThat((tokens[0] as ExpressionParser.NumberToken).value, equalTo(BigDecimal("-123.4567")))
+        assertThat((tokens[1] as ExpressionParser.UnitsToken).value, equalTo(Units.IN))
+    } 
+
+    @Test fun complicatedExpression() {
+        val tokens = parser.tokenize("  -123.4567mm+ -.1cm - (( ( 4cm --1cm)) -0.4567mm )  ")
+//        assertThat(tokens.size, equalTo(2)) 
+        val i = tokens.iterator()
+        assertThat((i.next() as ExpressionParser.NumberToken).value, equalTo(BigDecimal("-123.4567")))
+        assertThat((i.next() as ExpressionParser.UnitsToken).value, equalTo(Units.MM))
+        assertThat(i.next() as ExpressionParser.Plus, anything())
+        assertThat((i.next() as ExpressionParser.NumberToken).value, equalTo(BigDecimal("-0.1")))
+        assertThat((i.next() as ExpressionParser.UnitsToken).value, equalTo(Units.CM))
+        assertThat(i.next() as ExpressionParser.Minus, anything())
+        assertThat(i.next() as ExpressionParser.OpenBracket, anything())
+        assertThat(i.next() as ExpressionParser.OpenBracket, anything())
+        assertThat(i.next() as ExpressionParser.OpenBracket, anything())
+        assertThat((i.next() as ExpressionParser.NumberToken).value, equalTo(BigDecimal("4")))
+        assertThat((i.next() as ExpressionParser.UnitsToken).value, equalTo(Units.CM))
+        assertThat(i.next() as ExpressionParser.Minus, anything())
+        assertThat((i.next() as ExpressionParser.NumberToken).value, equalTo(BigDecimal("-1")))
+        assertThat((i.next() as ExpressionParser.UnitsToken).value, equalTo(Units.CM))
+        assertThat(i.next() as ExpressionParser.CloseBracket, anything())
+        assertThat(i.next() as ExpressionParser.CloseBracket, anything())
+        assertThat(i.next() as ExpressionParser.Minus, anything())
+        assertThat((i.next() as ExpressionParser.NumberToken).value, equalTo(BigDecimal("0.4567")))
+        assertThat((i.next() as ExpressionParser.UnitsToken).value, equalTo(Units.MM))
+        assertThat(i.next() as ExpressionParser.CloseBracket, anything())
+        
+        assertFalse(i.hasNext())
+    } 
 }
